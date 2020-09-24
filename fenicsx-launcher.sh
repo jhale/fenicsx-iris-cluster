441b5194d8c4688b179edb0f2c3ffc5084bbe5ba
Jack S. Hale <mail@jackhale.co.uk>
2020-09-24 16:43:55 +0200

    Fix path.

--- fenicsx-launcher.sh
+++ fenicsx-launcher.sh
@@ -2,6 +2,6 @@
 #SBATCH --time=0-00:03:00
 #SBATCH --ntasks=8
 #SBATCH --qos=qos-batch
-source $HOME/fenicsx-gaia-cluster/env-fenics.sh
+source $HOME/fenicsx-iris-cluster/env-fenics.sh
 cd $SLURM_SUBMIT_DIR
 srun --mpi=pmi2 $@ 
