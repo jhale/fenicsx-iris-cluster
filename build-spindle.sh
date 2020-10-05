1b46776498a498286733939f985cfd65d14f8a6e
Jack S. Hale <mail@jackhale.co.uk>
2020-10-05 10:45:14 +0200

    Explicitly add munge security option.

--- build-spindle.sh
+++ build-spindle.sh
@@ -8,6 +8,6 @@ cd ${BUILD_DIR} && \
     git clone https://github.com/hpc/Spindle.git && \
     cd Spindle && \
     ./bootstrap.sh && \
-    ./configure CC=gcc CXX=g++ BE_CC=gcc BE_CXX=g++ --with-rm=slurm --with-localstorage=/tmp --prefix=${PREFIX} && \
+    ./configure CC=gcc CXX=g++ BE_CC=gcc BE_CXX=g++ --with-rm=slurm --with-localstorage=/tmp --prefix=${PREFIX} --enable-sec-munge && \
     make && \
     make install
