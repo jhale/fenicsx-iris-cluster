7001c270c6dc81158767fde377a7fe358356538c
Jack S. Hale <mail@jackhale.co.uk>
2020-10-05 10:16:08 +0200

    Test run.

---
+++ build-spindle.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+source env-build-fenics.sh
+
+mkdir -p $BUILD_DIR
+
+cd ${BUILD_DIR} && \
+    git clone https://github.com/hpc/Spindle.git && \
+    cd Spindle && \
+    ./bootstrap.sh && \
+    ./configure CC=gcc CXX=g++ BE_CC=gcc BE_CXX=g++ --with-rm=slurm --with-localstorage=/tmp --prefix=${PREFIX} && \
+    make && \
+    make install
