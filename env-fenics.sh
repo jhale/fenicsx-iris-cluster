6e6a5dfe9e062cd3240b68e7fb2ca48f42125dda
Jack S. Hale <mail@jackhale.co.uk>
2020-10-14 15:29:20 +0200

    Up JIT flags to something more aggressive.

--- env-fenics.sh
+++ env-fenics.sh
@@ -4,6 +4,7 @@ source ${HOME}/fenicsx-iris-cluster/env-build-fenics.sh
 # Note: These must be unset initially, need to code this in somehow.
 export PETSC_DIR=${PREFIX}
 export DOLFINX_JIT_CACHE_DIR=${PREFIX}/cache
+export DOLFINX_JIT_CFLAGS="-O3 -march=native"
 
 # Bring in virtualenv with python package
 source $HOME/.local/bin/virtualenvwrapper.sh
