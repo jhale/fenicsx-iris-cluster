23393cb40e1a3383f8f910117834a9505ec7b76a
Michal Habera michal.habera.001@student.uni.lu <mhabera@access1.iris-cluster.uni.lux>
2020-11-26 08:51:10 +0100

    Native build on Haswell

--- fenicsx-launcher.sh
+++ fenicsx-launcher.sh
@@ -2,5 +2,5 @@
 #SBATCH --time=0-00:03:00
 source $HOME/fenicsx-iris-cluster/env-fenics.sh
 cd $SLURM_SUBMIT_DIR
-#srun --mpi=pmi2 "$@"
-spindle srun --mpi=pmi2 "$@" 
+srun --mpi=pmi2 "$@"
+#spindle srun --mpi=pmi2 "$@" 
