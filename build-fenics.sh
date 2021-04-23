3121bdff5477cde55368cb16dafcc422f730bedb
Jack S. Hale <mail@jackhale.co.uk>
2021-04-23 17:12:15 +0200

    Don't use xsimd for now.

--- build-fenics.sh
+++ build-fenics.sh
@@ -14,7 +14,7 @@ cd $BUILD_DIR && \
    git clone --depth 1 https://github.com/fenics/basix.git && \
    cd basix && \
    cmake -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE=${FLAGS} \
-     -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_VENDOR="Intel10_64_dyn" -DXTENSOR_USE_XSIMD=ON \
+     -DCMAKE_INSTALL_PREFIX=${PREFIX} -DBLA_VENDOR="Intel10_64_dyn" \
      -B build-dir -S . && \
    cmake --build build-dir -- -j8 && \
    cmake --install build-dir && \
@@ -30,10 +30,10 @@ cd $BUILD_DIR && \
    cd build && \
    cmake -DDOLFINX_SKIP_BUILD_TESTS=True -DCMAKE_INSTALL_PREFIX=${PREFIX} \
      -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE=${FLAGS} \
-     -DBLA_VENDOR="Intel10_64_dyn" -DXTENSOR_USE_XSIMD=ON ../cpp && \
+     -DBLA_VENDOR="Intel10_64_dyn" ../cpp && \
    make -j8 install && \
    cd ../python && \
    CXXFLAGS=${FLAGS} python3 -m pip install --ignore-installed --no-dependencies .
 
 mkdir -p ~/.config/dolfinx
-echo "{ 'cache_dir': '${SCRATCH}fenicsx-jit-cache', 'cffi_extra_compile_args': '-O3 -march=broadwell' }" > ~/.config/dolfinx/dolfinx_jit_parameters.json 
+echo "{ 'cache_dir': '${SCRATCH}fenicsx-jit-cache', 'cffi_extra_compile_args': ${FLAGS} }" > ~/.config/dolfinx/dolfinx_jit_parameters.json 
