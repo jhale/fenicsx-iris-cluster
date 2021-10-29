de2c629fb57939060e02329291b189bb058dbc03
Jack S. Hale <mail@jackhale.co.uk>
2021-10-29 11:51:17 +0200

    Update build-python-modules.sh

--- build-python-modules.sh
+++ build-python-modules.sh
@@ -11,6 +11,7 @@ python3 -m venv ${PREFIX}/python-venv
 source ${PREFIX}/python-venv/bin/activate
 python3 -m pip install -v wheel 
 
+python3 -m pip install --no-binary pybind11
 python3 -m pip install --no-binary numpy numpy 
 python3 -m pip install --no-cache-dir cython 
 python3 -m pip install --no-cache-dir llvmlite numba
