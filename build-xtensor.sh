a1e6a9ae54498fa8cb8f098f3ca15c76a84b2558
Jack S. Hale <mail@jackhale.co.uk>
2022-09-05 17:23:18 +0200

    Fix

--- build-xtensor.sh
+++ build-xtensor.sh
@@ -10,8 +10,8 @@ mkdir -p $BUILD_DIR
 cd ${BUILD_DIR} && \
    wget --no-check-certificate --read-timeout=10 -nc https://github.com/xtensor-stack/xtl/archive/refs/tags/${XTL_VERSION}.tar.gz -O xtl.tar.gz && \
    mkdir -p ${BUILD_DIR}/xtl && \
-   tar -xf xtl.tar.gz -C ${BUILD_DIR}/xtensor --strip-components=1 && \
-   cd xtensor && \
+   tar -xf xtl.tar.gz -C ${BUILD_DIR}/xtl --strip-components=1 && \
+   cd xtl && \
    cmake -G Ninja -DCMAKE_BUILD_TYPE="Release" -DCMAKE_CXX_FLAGS_RELEASE="${FLAGS}" \
      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
      -B build-dir -S . && \
