91e68f3b53e1e4705180c8a74cb4489951343e4b
rosecollet <72610763+rosecollet@users.noreply.github.com>
2023-09-19 13:42:05 +0200

    Update build-gmsh.sh

--- build-gmsh.sh
+++ build-gmsh.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -l
+set -e
+source env-build-fenics.sh
+
+VERSION="3.25.3"
+
+mkdir -p $BUILD_DIR
 
 RUN git clone -b gmsh_${GMSH_VERSION} --single-branch --depth 1 https://gitlab.onelab.info/gmsh/gmsh.git && \
-    cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_BUILD_DYNAMIC=1  -DENABLE_OPENMP=1 -B build-dir -S gmsh && \
+    cmake -G Ninja -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -DENABLE_BUILD_DYNAMIC=1  -DENABLE_OPENMP=1 -B build-dir -S gmsh && \
     cmake --build build-dir && \
     cmake --install build-dir && \
     rm -rf /tmp/*
