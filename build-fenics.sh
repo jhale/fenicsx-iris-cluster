71e3359052beaac3560e461ded5f137dc96ffbd5
Jack S. Hale <mail@jackhale.co.uk>
2023-10-10 12:09:18 +0200

    New tag for DOLFINx.

--- build-fenics.sh
+++ build-fenics.sh
@@ -25,7 +25,7 @@ python3 -m pip install -v --no-cache-dir git+https://github.com/FEniCS/ffcx.git@
 
 unset I_MPI_PMI_LIBRARY # Necessary if running in interactive session
 cd ${BUILD_DIR} && \
-   git clone -b v0.7.0 --single-branch --depth 1 https://github.com/fenics/dolfinx.git && \
+   git clone -b v0.7.0.post0 --single-branch --depth 1 https://github.com/fenics/dolfinx.git && \
    cd dolfinx/cpp && \
    cmake -G Ninja -B build-dir -S . \
      -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE="Release" \
